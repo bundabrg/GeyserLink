db6de50b9c08f41d1c7bff603456fc2e73ab4e31
bundabrg <brendan@grieve.com.au>
2020-06-19 12:33:05 +0800

    Restructure Project to group platforms and add a common module

--- platform/spigot/src/main/java/au/com/grieve/geyserlink/spigot/GeyserLink.java
+++ platform/spigot/src/main/java/au/com/grieve/geyserlink/spigot/GeyserLink.java
@@ -25,6 +25,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.plugin.messaging.PluginMessageListener;
+import org.jetbrains.annotations.NotNull;
 
 
 public final class GeyserLink extends JavaPlugin implements Listener, PluginMessageListener {
@@ -60,7 +61,7 @@ public final class GeyserLink extends JavaPlugin implements Listener, PluginMess
     }
 
     @Override
-    public void onPluginMessageReceived(String channel, Player player, byte[] bytes) {
+    public void onPluginMessageReceived(@NotNull String channel, @NotNull Player player, @NotNull byte[] bytes) {
         getLogger().warning("Message[" + channel + "]: " + new String(bytes));
     }
 }
